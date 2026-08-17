f9552fd72f018dd21856d6fdfb2ba7b1a6b2f33f
Yan W. Portero Guzman <YEYE2e@users.noreply.github.com>
2026-08-16 21:59:43 -0500

    feat: add GitHub Pages deployment workflow and configure site base path for sub-directory hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yeye2e.github.io',
+  base: '/Portfolio',
   compressHTML: true
 });
